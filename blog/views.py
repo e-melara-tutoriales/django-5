5bf6b04298f4c4ef7cdf36d42c54a7456f890815
Edwin Melara <edwin.melara@incaf.gob.sv>
2024-10-04 11:43:42 -0600

    Detalles del post

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, post_id):
+    print('post_detail {}'.format(post_id))
     post = get_object_or_404(
         Post,
         id = post_id,
