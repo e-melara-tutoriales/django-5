3754048a17ca1199b4f59f45db03152ce692c580
Edwin Melara <edwin.melara@incaf.gob.sv>
2024-10-04 13:39:24 -0600

    Agregando paginacion

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 3)
+    page_numer = request.GET.get('page', 1)
+    posts_list = paginator.page(page_numer)
 
-def post_detail(request, post_id):
-    print('post_detail {}'.format(post_id))
+    return render(request, 'blog/post/list.html', {
+        'posts': posts_list
+    })
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id = post_id,
         status = Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
-
     return render(request, 'blog/post/detail.html', {'post': post})
