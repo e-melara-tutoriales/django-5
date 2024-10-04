74314a76500be22cae36eec9dd81b2d5221c7bed
Edwin Melara <edwin.melara@incaf.gob.sv>
2024-10-04 11:14:53 -0600

    Mas cambios en los modelos

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super(PublishedManager, self).get_queryset()
+                .filter(status=Post.Status.PUBLISHED))
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -25,6 +30,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
